6ba4d45a2e53ab93167b510076c0f13dd5d7ecbc
Israel Antunes <49755889+israelantunes@users.noreply.github.com>
2023-10-12 19:21:48 +0000

    Uploading the new home page

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ function Home() {
   return (
     <h1>
       {" "}
-      Jocy, agora vai, se não for dessa vez que eu construo um site que me deixa
-      milionário eu desisto ok?{" "}
+      Em Construção!
     </h1>
   );
 }
